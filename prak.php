5f6e61f5900c14bd8835eafc56a65ccafafab939
Devandio Naufal Zuhair <92972902+devandioo@users.noreply.github.com>
2022-02-26 06:48:04 -0800

    Add files via upload

---
+++ prak.php
@@ -0,0 +1,16 @@
+<?php
+$nama = 'devan';
+$umur = 19;
+$berat = 52;
+
+echo "Nama Saya $nama, Umur saya $umur, berat saya $berat";
+echo "<br />";
+
+//variabel
+echo 'Dokumen Root '.$_SERVER["DOCUMENT_ROOT"];
+echo "<br />";
+
+//variabel konstan
+define('makanan'. 'sushi' );
+echo makanan;
+?>
